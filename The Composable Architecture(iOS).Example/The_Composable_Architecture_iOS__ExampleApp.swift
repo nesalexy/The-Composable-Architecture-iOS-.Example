d2ad16194d9af9bf7ef5424429374406bf8a3568
nesalexy <nesalexy@gmail.com>
2024-06-07 17:57:30 +0200

    init

---
+++ The Composable Architecture(iOS).Example/The_Composable_Architecture_iOS__ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  The_Composable_Architecture_iOS__ExampleApp.swift
+//  The Composable Architecture(iOS).Example
+//
+//  Created by Alexy Nesterchuk on 07.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct The_Composable_Architecture_iOS__ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
