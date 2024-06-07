96df7369ee4c2d8af952a5ea34e40c84b642e511
nesalexy <nesalexy@gmail.com>
2024-06-07 19:12:49 +0200

    connect CounterFeature with CounterView

--- The Composable Architecture(iOS).Example/CounterFeature.swift
+++ The Composable Architecture(iOS).Example/CounterFeature.swift
@@ -6,11 +6,11 @@
 //
 
 import ComposableArchitecture
-
+import SwiftUI
 
 @Reducer
 struct CounterFeature {
-    struct State {
+    struct State: Equatable {
         var count = 0
     }
     
@@ -35,4 +35,54 @@ struct CounterFeature {
             }
         }
     }
+}
+
+struct CounterView: View {
+    let store: StoreOf<CounterFeature>
+    
+    var body: some View {
+        WithViewStore(self.store, observe: { $0 }) { viewStore in
+            VStack {
+                Text("\(viewStore.count)")
+                    .font(.largeTitle)
+                    .padding()
+                    .background(Color.black.opacity(0.1))
+                    .cornerRadius(10)
+                HStack {
+                    Button("-") {
+                        viewStore.send(.decrementButtonTapped)
+                    }
+                    .font(.largeTitle)
+                    .padding()
+                    .background(Color.black.opacity(0.1))
+                    .cornerRadius(10)
+                    
+                    Button("+") {
+                        viewStore.send(.incrementButtonTapped)
+                    }
+                    .font(.largeTitle)
+                    .padding()
+                    .background(Color.black.opacity(0.1))
+                    .cornerRadius(10)
+                    
+                    Button("Reset") {
+                        viewStore.send(.resetButtonTapped)
+                    }
+                    .font(.largeTitle)
+                    .padding()
+                    .background(Color.black.opacity(0.1))
+                    .cornerRadius(10)
+                }
+            }
+        }
+    }
+}
+
+
+#Preview {
+  CounterView(
+    store: Store(initialState: CounterFeature.State()) {
+      CounterFeature()
+    }
+  )
 }
